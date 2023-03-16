735a31160512562eb973f6f00d677d0addf2f3a6
lma <loic.magnette@oniryx.be>
2023-03-16 11:06:20 +0100

    #6 add 2 route on same path

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserListV2Component } from './user-list-v2/user-list-v2.component';
 
 export const ADMIN_ROUTES:Routes =[
   { path: '', component: UserListComponent},
-  //{ path: '', component: UserListV2Component}
+  { path: '', component: UserListV2Component}
 ]
