b4f58c8afa9994a9d69558f11d15dd1decb0c65f
lma <loic.magnette@oniryx.be>
2023-03-16 11:35:18 +0100

    clean up

--- src/app/features/admin/admin-as-function.guard.ts
+++ src/app/features/admin/admin-as-function.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivateFn, CanMatchFn } from '@angular/router';
+import { CanMatchFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { UserService } from './user.service';
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter, Routes } from '@angular/router';
